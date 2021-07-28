0713d08e01faa875aecc676b47b8b3aeb85b9ade
sword <sword@10.0.0.105>
2021-07-28 13:48:28 -0300

    erros

--- src/Controler/Main.java
+++ src/Controler/Main.java
@@ -7,7 +7,7 @@ import View.Viewer;
 public class Main {
     
     public static void main(String[] args) {
-        
+        //ghgh
         Control controle = new Control();
         //Viewer mostrar = new Viewer();
         controle.printMenu();
